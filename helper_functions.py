edd03c852a5d32155e2b66b2336b0f2b5f349cb9
djdumpling <alexwa2024@gmail.com>
2025-06-30 00:09:24 -0700

    cartpole ppo: helper functions and first half of learning phase

---
+++ helper_functions.py
@@ -0,0 +1,49 @@
+import torch as t
+import random
+import numpy as np
+import gymnasium as gym
+
+def set_global_seeds(seed):
+    t.manual_seed(seed)
+    t.cuda.manual_seed_all(seed)
+    random.seed(seed)
+    np.random.seed(seed)
+    t.backends.cudnn.deterministic = True
+
+def make_env(env_id, seed, idx, run_name, mode, video_log_freq, video_save_path):
+    def thunk():
+        env = gym.make(env_id, render_mode = "rgb_array")
+        env = gym.wrappers.RecordEpisodeStatistics(env)
+        
+        if idx == 0 and video_log_freq:
+            env = gym.wrappers.RecordVideo(
+                env, 
+                f"{video_save_path}/{run_name}", 
+                episode_trigger = lambda episode_id: episode_id & video_log_freq == 0,
+                disable_logger = True
+            )
+
+        # if mode == "atari":
+        #     env = prepare_atari_env(env)
+        # elif mode == "mujoco":
+        #     env = prepare_mujoco_env(env)
+
+        env.reset(seed = seed)
+        env.action_space.seed(seed)
+        env.observation_space.seed(seed)
+
+        return env
+    
+    return thunk
+
+def get_episode_data_from_infos(infos: dict) -> dict[str, int | float] | None:
+    """
+    Helper function: returns dict of data from the first terminated environment, if at least one terminated.
+    """
+    for final_info in infos.get("final_info", []):
+        if final_info is not None and "episode" in final_info:
+            return {
+                "episode_length": final_info["episode"]["l"].item(),
+                "episode_reward": final_info["episode"]["r"].item(),
+                "episode_duration": final_info["episode"]["t"].item(),
+            }
